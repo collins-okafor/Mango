168724277f397fc46bf29a635bb575c0fbdf8009
Collins Okafor <69325061+collins-okafor@users.noreply.github.com>
2023-07-23 13:06:34 +0100

    service

---
+++ Mango.Web/Service/BaseService.cs
@@ -0,0 +1,24 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+
+namespace Mango.Web.Service
+{
+    public class BaseService : IBaseService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
+        {
+            HttpClient client = _httpClientFactory.CreateClient("MangoAPI");
+            HttpRequestMessage message = new();
+            message.Headers.Add("Content-Type", "application/json");    
+            // token
+
+            message.RequestUri = new Uri(requestDto.Url);
+        }
+    }
+}
