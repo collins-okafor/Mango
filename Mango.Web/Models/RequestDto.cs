2a7a0ff52d4d5ab8f27ce8062bf69cdade2c4645
Collins Okafor <69325061+collins-okafor@users.noreply.github.com>
2023-07-23 12:55:14 +0100

    service

--- Mango.Web/Models/RequestDto.cs
+++ Mango.Web/Models/RequestDto.cs
@@ -6,7 +6,7 @@ namespace Mango.Web.Models
     {
         public ApiType ApiType { get; set; } = ApiType.GET;
         public string? Url { get; set; }
-        public object Data { get; set; }
-        public string AccessToken { get; set; }                               
+        public object? Data { get; set; }
+        public string? AccessToken { get; set; }                               
     }
 }
