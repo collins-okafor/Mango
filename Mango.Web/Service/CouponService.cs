5cbfaecf8d8f57a31a78871a61f1e02a044677c4
Collins Okafor <69325061+collins-okafor@users.noreply.github.com>
2023-07-24 05:59:10 +0100

    GetByCode

--- Mango.Web/Service/CouponService.cs
+++ Mango.Web/Service/CouponService.cs
@@ -37,7 +37,7 @@ namespace Mango.Web.Service
             return await _baseService.SendAsync(new RequestDto()
             {
                 ApiType = SD.ApiType.GET,
-                Url = SD.CouponAPIBase + "/api/coupon"
+                Url = SD.CouponAPIBase + "/api/coupon/GetByCode"+couponCode
             });
         }
 
