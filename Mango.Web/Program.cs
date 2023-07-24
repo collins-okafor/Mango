bee65147014d12ad26e8d7a49b0b4dbc8667953b
Collins Okafor <69325061+collins-okafor@users.noreply.github.com>
2023-07-24 05:43:32 +0100

    configuration

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -1,3 +1,5 @@
+using Mango.Web.Service;
+using Mango.Web.Service.IService;
 using Mango.Web.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddHttpClient();
+builder.Services.AddHttpClient<ICouponService, CouponService>();
 
 SD.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"];
 
