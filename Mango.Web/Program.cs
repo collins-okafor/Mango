99bb1e6e270e932377523ad97fac154200411546
Collins Okafor <69325061+collins-okafor@users.noreply.github.com>
2023-07-24 05:41:32 +0100

    configuration

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -4,6 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddHttpClient();
 
 SD.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"];
 
